4dc7d19fce958f8f5f98e725216e4259a75f7675
susan <ngesasuzan@gmail.com>
2024-11-13 11:29:24 +0300

    icon updates

--- app/src/main/java/com/kima/empapp/DeleteActivity.kt
+++ app/src/main/java/com/kima/empapp/DeleteActivity.kt
@@ -1,10 +1,13 @@
 package com.kima.empapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
 
 class DeleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,15 @@ class DeleteActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val idedittext =findViewById<EditText>(R.id.id_number)
+        val btndelete =findViewById<Button>(R.id.btndelete)
+        btndelete.setOnClickListener {
+            val helper =  ApiHelper(applicationContext)
+            val api ="https://susan99.pythonanywhere.com/employees"
+            val body = JSONObject()
+            body.put("id",idedittext.text.toString())
+            helper.delete(api,body)
+        }
+        
     }
 }
--- app/src/main/java/com/kima/empapp/PutActivity.kt
+++ app/src/main/java/com/kima/empapp/PutActivity.kt
@@ -1,10 +1,13 @@
 package com.kima.empapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
 
 class PutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,17 @@ class PutActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val idedittext =findViewById<EditText>(R.id.empid)
+        val nameedittext = findViewById<EditText>(R.id.empname)
+        val btnupdate =findViewById<Button>(R.id.btnupdate)
+        btnupdate.setOnClickListener {
+            val helper =  ApiHelper(applicationContext)
+            val api ="https://susan99.pythonanywhere.com/employees"
+            val body = JSONObject()
+            body.put("id",idedittext.text.toString())
+            body.put("firstname",nameedittext.text.toString())
+            helper.put(api,body)
         }
     }
 }
